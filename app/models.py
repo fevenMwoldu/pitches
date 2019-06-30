c411c73fb95aade56e062d4af4a4db71ed54d50f
feven <feven.m.woldu@gmail.com>
2019-06-30 08:31:37 +0300

    modified my index.html

--- app/models.py
+++ app/models.py
@@ -1,43 +1,78 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+# from . login_manager
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    password = db.Column(db.String(255))
-    fullname = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True,index = True)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
+    comments = db.relationship('Comment', backref='user', lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_secure, password)
 
     def __repr__(self):
         return f'User {self.username}'
 
-class Category(db.Model):
-    __tablename__ = 'category'
-    id = db.Column(db.Integer,primary_key = True)
-    category_name = db.Column(db.String(255))
+
+class Role(db.Model):
+    __tablename__ = 'role'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref='role', lazy="dynamic")
 
     def __repr__(self):
-        return f'User {self.category_name}'
+        return f'Role {self.name}'
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='category', lazy="dynamic")
+
+    def __repr__(self):
+        return f'Category {self.category_name}'
+
 
 class Pitch(db.Model):
     __tablename__ = 'pitch'
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255))
-    category_id = db.Column(db.Integer)
-    user_id = db.Column(db.Integer)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     upvotes = db.Column(db.Integer)
     downvotes = db.Column(db.Integer)
+    comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
 
     def __repr__(self):
-        return f'User {self.content}'
+        return f'Pitch {self.content}'
+
 
 class Comment(db.Model):
     __tablename__ = 'comment'
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255))
-    user_id = db.Column(db.Integer)
-    pitch_id = db.Column(db.Integer)
-    
-    def __repr__(self):
-        return f'User {self.content}'
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitch.id"))
 
+    def __repr__(self):
+        return f'Comment {self.content}'
